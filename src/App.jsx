de575bdbad3896f32680b1a9fd62872383320dd8
Pedro Pires <pepepires@hotmail.com>
2025-06-21 02:37:28 +0100

    Now it works

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { NotFound } from "./pages/NotFound"
 
