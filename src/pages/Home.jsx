ca37e1f1ba739b99562a922e9acc1076df2c3d96
Pedro Pires <pepepires@hotmail.com>
2025-06-22 02:30:27 +0100

    Moon and Sun not working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = ()  => {
-    return <div className="min-h-screen">
+    return (
+         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
     {/*---------- Theme Toggle ---------*/}
+    <ThemeToggle />
 
     {/*---------- Background Effects ---------*/}
 
@@ -12,4 +16,5 @@ export const Home = ()  => {
     {/*---------- Footer ---------*/}
 
     </div>
-}
+    );
+};
