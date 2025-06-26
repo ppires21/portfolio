eeac5d24800a1c1be1e82271907a8d5564238da7
Pedro Pires <pepepires@hotmail.com>
2025-06-26 16:27:17 +0100

    Errors showing up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,9 +13,29 @@ const navItems = [
 export const Navbar = () => {
 
 const [isScrolled, setIsScrolled] = useState(false);
-useEffect ()
+
+useEffect( () =>{
+    const handleScroll = () => {
+        setIsScrolled(window.screenY > 10)
+    }
+
+    window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+}, []);
 
     return (
-    <nav className={cn("fixed 2-full z-40 transition-all duration-300", )}></nav>
-)
-}
+        <nav className={cn("fixed 2-full z-40 transition-all duration-300", isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5")}>
+
+            <div className="container flex items-center justify-between">
+                <a className = "text-xl font-bold text-primary flex items-center">
+                    <span className = "relative z-10">
+                        {" "}
+                        <span className = "text-glow text-foreground"> Pedro Pires </span> Portfolio
+                    </span>
+                </a>
+
+            </div>
+
+        </nav>
+    );
+};
