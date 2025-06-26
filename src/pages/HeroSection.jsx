5bb93bc1d36ad681558caada671466a58812b39d
Pedro Pires <pepepires@hotmail.com>
2025-06-26 17:51:46 +0100

    Error in the HeroSection

---
+++ src/pages/HeroSection.jsx
@@ -0,0 +1,21 @@
+
+
+export const HeroSection = () => {
+    return <section id="hero" className = "relative min-h-screen flex flex-col items-center justify-center px-4"
+    >
+        <div className="container max-w-4xl mx-auto text-center z-10">
+            <div className = "space-y-6">
+
+                <h1>
+                    <span> Hi, I'm </span>
+                    <span> Pedro </span>
+                    <span> Pires </span>
+                </h1>
+
+            </div>
+
+
+        </div>
+
+    </section>
+}
