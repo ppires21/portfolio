d3dc627001d4504228a7adb6f8ab177b8481e96e
Pedro Pires <pepepires@hotmail.com>
2025-07-01 22:19:38 +0100

    Fixed

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -2,5 +2,16 @@
 
 
 export const ContactSection = () => {
-    return <section></section>;
+    return (
+        
+        <div className="container mx-auto max-w-5xl">
+            <section id="contact" className="py-24 px-4 relative bg-secondary/30">
+                <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+                    Get In <span className="text-primary"> Touch</span>
+                </h2>
+            </section>
+        </div>
+
+
+    );
 };
