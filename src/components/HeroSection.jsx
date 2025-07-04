be1a690940878d37eff83fcf19d8055c22d43dc9
Pedro Pires <pepepires@hotmail.com>
2025-07-04 04:16:05 +0100

    Tentativa de meter direito

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 import {ArrowDown} from "lucide-react";
 
 export const HeroSection = () => {
-    return <section id="hero" className = "relative min-h-screen flex flex-col items-center justify-center px-4"
+    return <section id="hero" className = "relative min-h-screen flex flex-col items-center justify-center px-4 pt-4"
     >
         <div className="container max-w-4xl mx-auto text-center z-10"> 
             <div className = "space-y-6">
